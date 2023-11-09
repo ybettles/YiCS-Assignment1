78d9d7efb550102cb4526854f3f9929c27aecfd7
Maisalae <Maisalae@users.noreply.github.com>
2023-11-09 17:41:37 +0000

    finished final question

--- assignment1.py
+++ assignment1.py
@@ -1,16 +1,16 @@
 def clean_up():
     """
-        f refers to text_to_clean.txt
-        sf refers to student_names.txt
-        use text to read in the appropriate file
-        cleaned is used store the wanted characters
-        :return: cleaned
-        """
+    f refers to text_to_clean.txt
+    sf refers to student_names.txt
+    use text to read in the appropriate file
+    cleaned is used store the wanted characters
+    :return: cleaned
+    """
     f = "text_to_clean.txt"
     sf = "student_names.txt"
     cleaned = ""
+
     # lower case char, upper case char, blank, full stop - valid characters
-    # insert code here to clean the file as per question 1
     ACCEPTED_CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ .\n"
     with open(f, 'r') as text:
         for line in text:
@@ -23,18 +23,25 @@ def clean_up():
     return cleaned
 
 def build_id():
-    """
+    '''
     f refers to the student_names.txt file created in clean_up()
     id_list is the list return with the id's created from the name / surname of each student
     :return: id_list
-    """
+    '''
     f = "student_names.txt"
     id_list = []
-    #insert code here to create the id's as per question 2
+
+    # reading in the names
     with open(f, 'r') as names:
         for name in names:
+            # splitting them into forename, middle name(s), and surname
             split_name = name.split(" ")
+            # getting initials of each name
             initials = [word[0].lower() for word in split_name]
+            """if they have three initials, great, thats the id
+            if they have two or one initials, fill the empty spaces with an x
+            if they have more than three initials, take the first two initials and the final initial.
+            all of these should be appended to the list """
             if len(initials) == 3:
                 id_list.append(initials[0] + initials[1] + initials[2])
             elif len(initials) == 2:
@@ -53,10 +60,10 @@ def validate_password(password):
     Validate the password to verify if it is legal or not as per Question 3
     There is a password.txt file given to you to verify invalid passwords
     :param password: make use of the password found in main(), the test file will also have additional passwords to test
-    :return: illegal_password
+    :return: illegal_password; a list of errors with the password provided. if no errors, list will be empty.
     """
+    # initialising variables and constants
     illegal_password = []
-    #insert code here to validate all the conditions of the password as per question 3
     LOWER = "abcdefghijklmnopqrstuvwxyz"
     UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     OTHER_ACCEPTED_CHARS = "0123456789_"
@@ -65,6 +72,7 @@ def validate_password(password):
     has_lower = False
     has_leading_digit = password[0].isdigit()
 
+    # checking that all the characters are valid, and seeing whether the case is mixed or not
     for char in password:
         if char not in LOWER and char not in UPPER and char not in OTHER_ACCEPTED_CHARS:
             invalid_chars = True
@@ -75,22 +83,29 @@ def validate_password(password):
 
     is_mixed_case = has_upper and has_lower
 
+    # opening and reading in the commonly used passwords to make sure they are not present in the password given
     with open("password.txt", 'r') as f:
         commonly_used = f.read().split("\n")
 
+    # length checks
     if len(password) < 8:
         illegal_password.append("TOO SHORT")
     if len(password) > 12:
         illegal_password.append("TOO LONG")
+    # character validity check
     if invalid_chars:
         illegal_password.append("WRONG CHARACTERS")
+    # mixed case check
     if not is_mixed_case:
         illegal_password.append("NOT MIXED CASE")
+    # checking to make sure leading character is not a digit
     if has_leading_digit:
         illegal_password.append("LEADING DIGIT")
+    # commonly used password check
     if password in commonly_used:
         illegal_password.append("CANNOT MAKE USE OF THIS PASSWORD")
 
+    # return list of errors (if none, list will be empty)
     return illegal_password
 
 
@@ -103,22 +118,32 @@ def create_unique(id_list):
     :return: final_list is returned and this list contains all of the unique student ids
     """
     final_list = []
-
+    # iterate over the id list
     for id in id_list:
+        # start with 0000, if that already exists (with same initials) then increment and try again, etc. until a unique one exists
         identifier = 0
         while True:
+            # create and format the id string
             id_string = f'{id}{identifier:04}'
             if id_string in final_list:
+                # increment the identifier number if that unique id already exists
                 identifier+=1
                 continue
+            elif f'{id}{9999}' in final_list:
+                # if somehow there are already 10,000 students with the same 3 character id, then no more unique
+                # identifiers are available with this formatting, so throw an error
+                raise Exception(f"Sorry but all unique identifiers for id: {id} are taken, please contact a system administrator for help.")
             else:
+                # if the id string is unique, then append it to the final list
                 final_list.append(id_string)
                 break
 
+    # opening the unique ids file in write mode to write the newly created ids in
     with open("unique_ids.txt", "w") as f:
         for id_string in final_list:
             f.write(id_string + "\n")
 
+    # opening the create emails file in write mode to write the student emails in
     with open("create_emails.txt", "w") as ef:
         for id_string in final_list:
             ef.write(id_string + "@student.bham.ac.uk\n")
@@ -136,15 +161,17 @@ def create_short_address():
     split_addrs = []
     ACCEPTED_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789, "
     with open(f, 'r') as text:
-        count = 0
+        # need to strip out the newline characters and since we aren't able to use .split, this is the easiest way to do it
         for line in text:
-            address =""
+            address = ""
             for char in line:
                 if char in ACCEPTED_CHARS:
+                    # adding the valid characters back in
                     address += char
             if address != "": # making sure any empty lines get ignored
                 # unpacking it because it's easier and then there's no off-by-one errors, also more readable
                 address1, address2, city, postcode = address.split(", ")
+                # appending only the first line and postcode
                 split_addrs.append([address1, postcode])
     return split_addrs
 
@@ -155,29 +182,40 @@ def validate_pcode(split_addrs):
     :param split_addrs: this is passed from main(), obtained from the function create_short_address()
     :return: validate_pcode is a list that contains True False values for each postcode that is validated - see question 6
     """
+    # initialising variables and constant
     validate_pcode = []
     UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     count = 0
+    # iterating through all the addresses
     for address in split_addrs:
+        # getting the postcode for the current address
         postcode = address[1]
+        # adding the index to the list
         validate_pcode.append(count)
+
+        # validating length
         if len(postcode) != 6:
+            # if it's too short replace it with dollar signs and append false to validate_pcode
             postcode = '$$$$$$'
             validate_pcode.append("False")
         else:
             validate_pcode.append("True")
         if postcode[0] not in UPPER:
+            # if the first character isn't an uppercase letter, append false
             validate_pcode.append("False")
         else:
             validate_pcode.append("True")
         if not (postcode[1].isdigit() and postcode[2].isdigit() and postcode[3].isdigit()):
+            # if the next three characters aren't numbers, append false
             validate_pcode.append("False")
         else:
             validate_pcode.append("True")
         if not (postcode[4] in UPPER and postcode[5] in UPPER):
+            # if the fifth and sixth characters are not uppercase letters, append false
             validate_pcode.append("False")
         else:
             validate_pcode.append("True")
+        # increment the index
         count+=1
 
     return validate_pcode
@@ -189,12 +227,17 @@ def ids_addrs(short_addr):
     :param short_addr: passed in from main() - generated from create_short_address()
     :return: combo is the dictionary, i.e. unique id is key, and the short addr for each student is value
     """
-    f = ""
-    ids = f.read()
+    # initialising the variable that will be needed
+    f = "unique_ids.txt"
     combo = {}
-
-
-
+    count = 0
+    # opening the file in read mode
+    with open(f, "r") as id_file:
+        # iterating through the lines of the file
+        for id in id_file:
+            # pairing each id (each line of the file) with each address from the list
+            combo[id] = short_addr[count]
+            count += 1
     return combo
 
 
